ee2cd5281011caf391770f3939668673d6584fe8
osso2k <ossoble817@gmal.com>
2025-07-11 15:08:48 +0300

    ConCol

---
+++ gazer.js
@@ -0,0 +1,12 @@
+const input = document.getElementById("input")
+const listC = document.querySelector("#listC")
+
+function addTask() {
+    if (input.value == "") {
+        alert("PLease enter a Task")
+    }
+    const newTask = document.createElement("li")
+    newTask.textContent = input.value
+    listC.append(newTask)
+    input.value = ""
+}
